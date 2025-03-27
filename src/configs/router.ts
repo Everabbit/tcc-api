723fc9045914cb972f7f5d5df8ab96971d2e249b
Everabbit <digitalwdl@gmail.com>
2025-03-26 22:38:09 -0300

    mais algumas coisas

--- src/configs/router.ts
+++ src/configs/router.ts
@@ -1,10 +1,13 @@
 import { Application } from 'express';
 import UserRoute from '../routes/user.route';
+import ProjectRoute from '../routes/project.route';
 
 export class ExpressRouter {
   private userRoute: UserRoute = new UserRoute();
+  private projectRoute: ProjectRoute = new ProjectRoute();
 
   public instanceRoutes(app: Application): void {
     this.userRoute.createInstances(app);
+    this.projectRoute.createInstances(app);
   }
 }
---
+++ src/routes/project.route.ts
@@ -0,0 +1,5 @@
+import { Application } from 'express';
+
+export default class ProjectRoute {
+  public createInstances(app: Application) {}
+}
